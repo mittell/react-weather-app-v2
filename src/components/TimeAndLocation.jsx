182322c0cde9a6c766085ae5ebac305908a7e1a7
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 19:45:40 +0900

    feat: added dynamic data references

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-function TimeAndLocation() {
+import { formatToLocalTime } from '../services/weatherService';
+
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
 	return (
 		<div>
 			<div className='flex items-center justify-center my-6'>
 				<p className='text-white text-xl font-extralight'>
-					Tuesday, 21 August 2022 | Local time: 15:00 PM
+					{formatToLocalTime(dt, timezone)}
 				</p>
 			</div>
 			<div className='flex items-center justify-center my-3'>
-				<p className='text-white text-3xl font-medium'>Nagoya, JP</p>
+				<p className='text-white text-3xl font-medium'>{`${name}, ${country}`}</p>
 			</div>
 		</div>
 	);
