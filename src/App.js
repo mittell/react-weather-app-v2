ecfbb723d2b6980da6a89fa62bf9ce88f14a9f68
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 15:40:19 +0900

    feat: added TemperatureAndDetails component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Inputs from './components/Inputs';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
 
@@ -7,8 +8,9 @@ function App() {
 		<div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
 			<TopButtons />
 			<Inputs />
-			
+
 			<TimeAndLocation />
+			<TemperatureAndDetails />
 		</div>
 	);
 }
