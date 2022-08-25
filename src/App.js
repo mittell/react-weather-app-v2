d808b6fd8d1d0a7438761791ae71eaa745c757e5
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 19:19:38 +0900

    feat: added weatherService

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import Inputs from './components/Inputs';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
+import getFormattedWeatherData from './services/weatherService';
 
 function App() {
+	const fetchWeather = async () => {
+		const data = await getFormattedWeatherData({ q: 'Nagoya' , units: "5",});
+
+		console.log(data);
+	};
+
+	fetchWeather();
+
 	return (
 		<div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
 			<TopButtons />
