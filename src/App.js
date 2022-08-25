364728a6328fd096a9a0327c45b2d13560927576
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 15:17:26 +0900

    feat: added TimeAndLocation component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
 
 function App() {
@@ -7,6 +7,8 @@ function App() {
 		<div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
 			<TopButtons />
 			<Inputs />
+			
+			<TimeAndLocation />
 		</div>
 	);
 }
