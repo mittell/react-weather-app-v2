7266ecfc59c1b477cc2941a912994e517accae3d
Chris Mittell <mittell@hotmail.co.uk>
2022-08-25 19:29:49 +0900

    feat: updated main logic

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
+import { useEffect, useState } from 'react';
+
 import Forecast from './components/Forecast';
 import Inputs from './components/Inputs';
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import TimeAndLocation from './components/TimeAndLocation';
 import TopButtons from './components/TopButtons';
+
 import getFormattedWeatherData from './services/weatherService';
 
 function App() {
-	const fetchWeather = async () => {
-		const data = await getFormattedWeatherData({ q: 'Nagoya' , units: "5",});
+	const [query, setQuery] = useState({ q: 'nagoya' });
+	const [units, setUnits] = useState('metric');
+	const [weather, setWeather] = useState(null);
 
-		console.log(data);
-	};
+	useEffect(() => {
+		const fetchWeather = async () => {
+			await getFormattedWeatherData(...query, ...units).then((data) => {
+				setWeather(data);
+			});
+		};
 
-	fetchWeather();
+		fetchWeather();
+	}, [query, units]);
 
 	return (
 		<div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
 			<TopButtons />
 			<Inputs />
 
-			<TimeAndLocation />
-			<TemperatureAndDetails />
+			{weather && (
+				<div>
+					<TimeAndLocation />
+					<TemperatureAndDetails />
 
-			<Forecast title='Hourly Forecast' />
-			<Forecast title='Daily Forecast' />
+					<Forecast title='Hourly Forecast' />
+					<Forecast title='Daily Forecast' />
+				</div>
+			)}
 		</div>
 	);
 }
